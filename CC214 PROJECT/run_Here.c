f5facf4cd7d75569a886c1adea9d36cd2bba6d4c
JOHNpCADAROO <126931681+JohnPerez012@users.noreply.github.com>
2025-01-06 11:18:49 +0800

    Add files via upload
    DSA final project

---
+++ CC214 PROJECT/run_Here.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include "Program/functions.h"
+#include "Program/homepage.h"
+#include "Program/loginNregister.h"
+#include "Program/nodesNprototypes.h"
+
+int main()
+{
+    menu();
+    return 0;
+}
